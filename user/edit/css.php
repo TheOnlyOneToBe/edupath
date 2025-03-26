e7c9d4c3d628969727cd8d1d57f87440108655b4
TheOnlyOneToBe <jkmarc8173@gmail.com>
2025-03-26 22:28:33 +0100

    asd

--- user/edit/css.php
+++ user/edit/css.php
@@ -1,18 +1,18 @@
 <!-- Bootstrap -->
 <link rel="stylesheet" href="../../assets/plugins/css/bootstrap.min.css" />
-    <!-- Animate CSS -->
-    <link rel="stylesheet" href="../../assets/plugins/css/animate.min.css" />
-    <!-- Owl Carousel CSS -->
-    <link rel="stylesheet" href="../../assets/plugins/css/owl.carousel.min.css" />
-    <!-- Maginific Popup CSS -->
-    <link rel="stylesheet" href="../../assets/plugins/css/maginific-popup.min.css" />
-    <!-- Nice Select CSS -->
-    <link rel="stylesheet" href="../../assets/plugins/css/nice-select.min.css" />
-    <!-- Icofont -->
-    <link rel="stylesheet" href="../../assets/plugins/css/icofont.css" />
-    <!-- Uicons -->
-    <link rel="stylesheet" href="../../assets/plugins/css/uicons.css" />
-    <!-- Main CSS -->
-    <link rel="stylesheet" href="../../style.css" />
-    <link rel="stylesheet" href="../../assets/plugins/fontawesome/css/all.css">
-    <link rel="stylesheet" href="../../assets/plugins/fontawesome/js/all.js">
+<!-- Animate CSS -->
+<link rel="stylesheet" href="../../assets/plugins/css/animate.min.css" />
+<!-- Owl Carousel CSS -->
+<link rel="stylesheet" href="../../assets/plugins/css/owl.carousel.min.css" />
+<!-- Maginific Popup CSS -->
+<link rel="stylesheet" href="../../assets/plugins/css/maginific-popup.min.css" />
+<!-- Nice Select CSS -->
+<link rel="stylesheet" href="../../assets/plugins/css/nice-select.min.css" />
+<!-- Icofont -->
+<link rel="stylesheet" href="../../assets/plugins/css/icofont.css" />
+<!-- Uicons -->
+<link rel="stylesheet" href="../../assets/plugins/css/uicons.css" />
+<!-- Font Awesome -->
+<link rel="stylesheet" href="../../assets/plugins/fontawesome/css/all.min.css" />
+<!-- Main CSS -->
+<link rel="stylesheet" href="../../style.css" />
